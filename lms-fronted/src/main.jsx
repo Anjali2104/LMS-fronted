aa233b0abf5398724a68096594698e96ed46ab03
Anjali2104 <anjaliu949@gmail.com>
2023-09-13 11:53:58 +0530

    redux setup with toolkit done

--- lms-fronted/src/main.jsx
+++ lms-fronted/src/main.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import { Toaster } from 'react-hot-toast'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-     <App />
-    </BrowserRouter>
+   
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+          <Toaster/>
+        </BrowserRouter>
+       </Provider>
     
+   
 
 )
