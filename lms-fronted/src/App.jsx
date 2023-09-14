3b2a33be2239f7ea11782f8261d10ed61c0ff0ac
Anjali2104 <anjaliu949@gmail.com>
2023-09-14 17:39:19 +0530

    added not found page

--- lms-fronted/src/App.jsx
+++ lms-fronted/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Aboutus from './pages/Aboutus'
 import Home from './pages/Home'
 import {Route, Routes} from 'react-router-dom'
+import Notfound from './pages/Notfound'
 function App() {
   
 
@@ -10,6 +11,7 @@ function App() {
    <Routes>
     <Route path='/' element={<Home/>}/>
     <Route path='/about' element={ <Aboutus/>} />
+    <Route path='*' element={<Notfound/>}/>
    </Routes>
   )
 }
