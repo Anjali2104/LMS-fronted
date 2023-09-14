8aecc607c36c89dea88ce21f9e4059f8d1c4ff02
Anjali2104 <anjaliu949@gmail.com>
2023-09-14 14:44:22 +0530

    added about us page

--- lms-fronted/src/App.jsx
+++ lms-fronted/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Aboutus from './pages/Aboutus'
 import Home from './pages/Home'
 import {Route, Routes} from 'react-router-dom'
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
    <Routes>
     <Route path='/' element={<Home/>}/>
+    <Route path='/about' element={ <Aboutus/>} />
    </Routes>
   )
 }
