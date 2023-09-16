cd197daeeadad88d23a4e0eef0b7d4419bd1c0f5
Anjali2104 <anjaliu949@gmail.com>
2023-09-16 21:21:13 +0530

    added the contact page

--- lms-fronted/src/App.jsx
+++ lms-fronted/src/App.jsx
@@ -6,6 +6,7 @@ import {Route, Routes} from 'react-router-dom'
 import Notfound from './pages/Notfound'
 import Signup from './pages/signup'
 import Signin from './pages/Signin'
+import Contact from './pages/Contact'
 function App() {
   
 
@@ -13,6 +14,7 @@ function App() {
    <Routes>
     <Route path='/' element={<Home/>}/>
     <Route path='/about' element={ <Aboutus/>} />
+    <Route path='/contact' element={<Contact/>}/>
     <Route path='/signup' element={<Signup/>}/>
     <Route path='/login' element={<Signin/>}/>
     <Route path='*' element={<Notfound/>}/>
